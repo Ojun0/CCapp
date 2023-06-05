91ad1f27aceb8b40a9d13170fc9310c42435c257
Ojun0 <ojy814@naver.com>
2023-06-05 14:14:56 +0900

    chatting 구현

--- app/src/main/java/com/example/cc/Roomlist.java
+++ app/src/main/java/com/example/cc/Roomlist.java
@@ -1,17 +1,24 @@
 package com.example.cc;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.cc.javapapers.android.chat.ChatArrayAdapter;
+import com.example.cc.javapapers.android.chat.ChatBubbleActivity;
+
 public class Roomlist extends AppCompatActivity {
     RelativeLayout main;
     EditText reserch;
+    Button room1, room2, room3, room4;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -34,15 +41,29 @@ public class Roomlist extends AppCompatActivity {
 //                main;
 //                return true;
         }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        setTitle("");
         main = (RelativeLayout) findViewById(R.id.main);
         reserch = (EditText) findViewById(R.id.reserch);
-    }
+        room1 = (Button) findViewById(R.id.room1);
+        room2 = (Button) findViewById(R.id.room2);
+        room3 = (Button) findViewById(R.id.room3);
+        room4 = (Button) findViewById(R.id.room4);
+
+        room1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ChatBubbleActivity.class);
+                startActivity(intent);
+            }
+        });
+   }
 
 
 }
